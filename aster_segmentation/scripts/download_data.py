004113f5053d2ab63054c52ffae9d91184d1cabd
jackstellwagen <jstellwagen19@students.polytechnic.org>
2019-08-07 14:55:11 -0700

    data can be downloaded if directories arent made first

--- aster_segmentation/scripts/download_data.py
+++ aster_segmentation/scripts/download_data.py
@@ -1,3 +1,5 @@
+import os
+import shutil
 try:
     from tensorflow.python.keras.utils.data_utils import get_file
 except ImportError:  # tf v1.9 moves conv_utils from _impl to keras.utils
@@ -6,5 +8,31 @@ except ImportError:  # tf v1.9 moves conv_utils from _impl to keras.utils
 
 "https://aster-segmentation.s3-us-west-2.amazonaws.com/15_frame_batch/Aster_fullsize_2D.npz"
 
-path = "../datasets/Aster_fullsize_2D.npz"
+
+if not os.path.exists("../datasets"):
+    os.mkdir("../datasets")
+
+path = "Aster_fullsize_2D.npz"
 path = get_file(path, origin="https://aster-segmentation.s3-us-west-2.amazonaws.com/15_frame_batch/Aster_fullsize_2D.npz")
+
+#print(path)
+shutil.move(path, "../datasets/Aster_fullsize_2D.npz")
+
+
+if not os.path.exists("../saved_networks"):
+    os.mkdir("../saved_networks")
+
+path = "conv_model_std_61.h5"
+path = get_file(path, origin="https://aster-segmentation.s3-us-west-2.amazonaws.com/15_frame_batch/trained_networks/conv_model_std_61.h5")
+
+shutil.move(path, "../saved_networks/conv_model_std_61.h5")
+
+
+
+path = "conv_model_std_61.npz"
+path = get_file(path, origin="https://aster-segmentation.s3-us-west-2.amazonaws.com/15_frame_batch/trained_networks/conv_model_std_61.npz")
+
+shutil.move(path,"../saved_networks/conv_model_std_61.npz")
+
+
+
